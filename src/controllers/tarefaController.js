af511fea08bda58e2bf09704c2d23a60f69743b0
rayssa <rayssagoncalves.tds2.senai@gmail.com>
2025-03-11 16:13:17 -0300

    feat: add create method

--- src/controllers/tarefaController.js
+++ src/controllers/tarefaController.js
@@ -10,26 +10,37 @@ class TarefaController {
       res.status(500).json({ erro: "Erro ao buscar tarefas" });
     }
   };
-  create = ({ body: { descricao } }, res) => {
+
+
+create = async (req, res) => {
+  const { descricao } = req.body;
+  try {
     if (!descricao) {
       return res.status(400).json({ erro: "Descrição é obrigatória" });
     }
-    const novaTarefa = tarefaModel.create(descricao);
+
+    const novaTarefa = await tarefaModel.create(descricao);
     res.status(201).json(novaTarefa);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ erro: "Erro ao criar tarefa" })
   };
-  update = ({ params: { id }, body: { concluida } }, res) => {
-    const tarefaAtualizada = tarefaModel.update(id, concluida);
-    if (!tarefaAtualizada) {
-      return res.status(404).json({ erro: "Tarefa não encontrada" });
-    }
-    res.json(tarefaAtualizada);
-  };
-  delete = ({ params: { id } }, res) => {
-    const sucesso = tarefaModel.delete(id);
-    if (!sucesso) {
-      return res.status(404).json({ erro: "Tarefa não encontrada" });
-    }
-    res.status(204).send();
-  };
+}
+
+update = ({ params: { id }, body: { concluida } }, res) => {
+  const tarefaAtualizada = tarefaModel.update(id, concluida);
+  if (!tarefaAtualizada) {
+    return res.status(404).json({ erro: "Tarefa não encontrada" });
+  }
+  res.json(tarefaAtualizada);
+};
+
+delete = ({ params: { id } }, res) => {
+  const sucesso = tarefaModel.delete(id);
+  if (!sucesso) {
+    return res.status(404).json({ erro: "Tarefa não encontrada" });
+  }
+  res.status(204).send();
+};
 }
 export default new TarefaController();
